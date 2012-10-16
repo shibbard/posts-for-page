ec2faca8828206ec3015ed4ee9e395b3e17db9fd
Simon Hibbard <hibbard.simon@gmail.com>
2012-10-16 17:45:14 +0100

    Intial commit

---
+++ posts-for-page.php
@@ -0,0 +1,312 @@
+<?php
+/*
+Plugin Name: Posts for Page Plugin
+Plugin URI: http://www.mywebdeveloperblog.com/development/wordpress/posts-for-page-wordpress-plugin
+Description: This plugin allows for posts to be assigned to pages as snippets or full posts. Posts can be selected by category slug, category id, tag slug, single post id, author and ordered by date or title.
+Version: 1.7
+Author: Simon Hibbard
+Author URI: http://www.mywebdeveloperblog.com/
+License: GPL2
+*/
+?>
+<?php
+
+//tell wordpress to register the children-excerpt shortcode
+add_shortcode("posts-for-page", "sc_posts_for_page");
+
+function remove_images( $content ) {
+   $postOutput = preg_replace('/<img[^>]+./','', $content);
+   return $postOutput;
+}
+
+function robins_get_the_excerpt($post_id) {
+  global $post;  
+  $save_post = $post;
+  $post = get_post($post_id);
+  $output = get_the_excerpt();
+  $post = $save_post;
+  //return preg_replace('/<a[^>]+>Continue reading.*?<\/a>/i','',$output);
+  return $output;
+}
+
+
+//children excerpt shortcode worker function
+function sc_posts_for_page($atts, $content = null){
+		
+	if($atts['length'] > 0 ){
+	}else{
+		$atts['length'] = 50;
+	}
+	//echo $atts['read_more'];
+	if($atts['read_more'] != "" ){
+		$_opts['readMoreText'] = $atts['read_more'];
+	}else{
+		$_opts['readMoreText'] = "Read More &raquo;";
+	}
+
+	if($atts['prev_text'] != "" ){
+		$_opts['prevText'] = $atts['prev_text'];
+	}else{
+		$_opts['prevText'] = "&laquo; Newer Entries";
+	}
+
+	if($atts['next_text'] != "" ){
+		$_opts['nextText'] = $atts['next_text'];
+	}else{
+		$_opts['nextText'] = "Older Entries &raquo; ";
+	}
+
+	// set values from the shortcode
+	$_opts['cat_slug'] = $atts['cat_slug'];
+	$_opts['cat'] = $atts['cat'];
+	$_opts['tag_slug'] = $atts['tag_slug'];
+	$_opts['order_by'] = $atts['order_by'];
+	$_opts['post_id'] = $atts['post_id'];
+	$_opts['author'] = $atts['author'];
+	$_opts['num'] = $atts['num'];
+	
+	$_opts['showFullPost'] = $atts['show_full_posts'];
+
+	global $paged;
+	
+	if (empty($paged)) {
+			$_opts['cur_page'] = 1;
+	}
+	else
+	{
+		$_opts['cur_page'] = $paged;
+	}
+
+	$_opts['hide_images'] = $atts['hide_images'];
+	$_opts['use_wp_excerpt'] = $atts['use_wp_excerpt'];
+	$_opts['strip_html'] = $atts['strip_html'];
+	$_opts['hide_post_content'] = $atts['hide_post_content'];
+	$_opts['show_meta'] = $atts['show_meta'];
+	$_opts['hide_post_title'] = $atts['hide_post_title'];
+    $_opts['hide_read_more'] = $atts['hide_read_more'];
+    $_opts['create_para_tags'] = $atts['create_para_tags'];
+    
+	//get the id of the current article that is calling the shortcode
+	$parent_id = get_the_ID();
+	
+	$output = "";
+	
+	$i = 0;
+	
+	$children  = pfp_get_posts($_opts);
+
+	if (is_array($children))
+	{
+		if(count($children) <= 0){
+			$out = "<strong>Items for Page:</strong> There are no posts that match the selection criteria.";
+			return $out;
+		}
+
+		foreach( $children as $child ) {
+			$title = $child->post_title;
+			$link = get_permalink($child->ID);
+			$content;
+			$date;
+			$thumb = get_the_post_thumbnail($child->ID,array(60, 60, true));
+			$imageSrc = "";
+
+            if($_opts['create_para_tags'] == "true")
+            {
+                $content = wpautop($child->post_content);
+                //echo "wpautop";
+            }
+            else
+            {
+                $content = $child->post_content;
+                //echo "no wpautop";
+            }
+            
+
+			if($_opts['showFullPost'] != "true")
+			{
+
+				if($_opts['hide_images'] != 'true')
+				{
+					$args = array(
+					'numberposts' => 1,
+					'order'=> 'ASC',
+					'post_mime_type' => 'image',
+					'post_parent' => $child->ID ,
+					'post_status' => null,
+					'post_type' => 'attachment'
+					);
+					//get the first image and resize it 
+					$attachments = get_children( $args );
+					//echo "here" .$_opts['imageH'];
+					if ($attachments) {
+						foreach($attachments as $attachment) {				
+							$image_attributes = wp_get_attachment_image_src( $attachment->ID, 'thumbnail' )  ? wp_get_attachment_image_src( $attachment->ID, 'thumbnail' ) : wp_get_attachment_image_src( $attachment->ID, 'full' );
+							$imageSrc = '<img src="'.wp_get_attachment_thumb_url( $attachment->ID ).'" />';
+						}
+					}	
+				}
+
+				if($_opts['use_wp_excerpt'] == 'true')
+				{					
+					// use wp excerpt function
+					//$post = get_post($post_id);
+					setup_postdata($child);
+					$content = robins_get_the_excerpt($child->ID);
+				}
+				else
+				{   
+					// remove all images from the post
+                    if($_opts['hide_images'] == 'true')
+                    {
+					    $content = remove_images($content);		
+                    }
+                    else
+                    {
+                        $content = ($content);		
+                    }
+          
+					if($_opts['strip_html'] == 'true')
+					{
+						$content = strip_tags($content);
+					}
+					//split excerpt into array for processing
+					$words = explode(' ', $content);
+			
+					//chop off the excerpt based on the atts->lenth
+					$words = array_slice($words, 0, $atts['length']);
+			
+					//merge the array of words for the excerpt back into sentances
+					$content = implode(' ', $words) . "...";	
+				}	
+			} 	
+			
+			// output post
+
+			$output .= "<div class='pfpItem entry-content'>";
+			
+			if($_opts['hide_post_title'] != 'true')
+			{
+				$output .= "<h2 class='entry-title'><a href='$link'>$title</a></h2>";
+			}
+			if($_opts['show_meta'] == 'true')
+			{
+				$userid = $child->post_author;
+				$userdata = get_userdata($userid); 
+				$output .= "<div class='entry-meta'>";
+				$output .= "Posted on " . mysql2date('F j, Y',$child->post_date) . " by " . $userdata->user_nicename;
+				$output .= "</div>";
+			}
+			// do not get any content if it is to be hidden (i.e. show titles only)
+			if($_opts['hide_post_content'] != 'true')
+			{
+				//if($_opts['hide_images'] != 'true')
+				//{
+					//$output .= $imageSrc;
+				//}
+				$output .= $content;
+                if( ($_opts['hide_read_more'] != 'true') && ($_opts['use_wp_excerpt'] != 'true'))
+                {    
+				    $output .= "<a href='$link' class='pfpReadMore'>" . $_opts['readMoreText'] . "</a>";
+                }
+			}
+			$output .= "<div class='clear'></div>";
+
+			$output .= "<hr>";				
+			$output .= "</div>";	
+									
+		}
+	}
+	
+    global $wp_query;
+    $page_links_total =  $wp_query->max_num_pages;
+
+	if($_opts['cur_page'] > 1)
+	{
+		// show prev
+		$output .= "<span class='pfpNav'>" . get_previous_posts_link($_opts['prevText']) . "</span>";
+	}
+	if($_opts['cur_page'] <  $page_links_total)
+	{
+		// show next
+		$output .= "<span class='pfpNav'>" . get_next_posts_link($_opts['nextText']) . "</span>";
+	}
+
+
+	wp_reset_query();
+	return $output;
+
+}
+	
+function pfp_get_posts($pfp_opts) {
+
+	$params = array();
+	if ($pfp_opts['post_id'] == '') { // for multiposts
+		
+		if ($pfp_opts['tag_slug'] != ''){
+			$params['tag_slug__in'] = explode(",", $pfp_opts['tag_slug']);
+		}
+		if ($pfp_opts['cat'] != ''){
+			$params['category__in'] =  explode(",", $pfp_opts['cat']);
+		}
+		if ($pfp_opts['cat_slug'] != '') {
+			$params['category_name'] = $pfp_opts['cat_slug'];
+		}
+		if($pfp_opts['author'] != '') {
+			$params['author'] = $pfp_opts['author'];
+		}
+		if($pfp_opts['order_by'] != '') {
+			$params['order_by'] = $pfp_opts['order_by'];
+		}
+		if($pfp_opts['num'] != '') {
+			//$params['numberposts'] = $pfp_opts['num'];
+			$params['posts_per_page'] = $pfp_opts['num'];
+			$params['paged'] = $pfp_opts['cur_page'];
+			//if($pfp_opts['page_num'] != '') {
+				// work out offset depending on page num
+				//$offset = $pfp_opts['page_num'] * $pfp_opts['num'];
+
+				//$params['offset'] = $offset;
+			//}
+		}
+		else
+		{
+			$params['posts_per_page'] =  -1; // gets them all
+		}
+		// apply whatever the case:
+		$params['suppress_filters'] = false;
+
+
+		// get the posts
+		//$postslist = get_posts($params);
+		$postslist = query_posts($params);
+
+	}else{ // for single posts
+		$postslist[0] = wp_get_single_post($pfp_opts['post_id']);
+	}
+	return $postslist;
+}
+
+
+//add_action('admin_menu', 'my_plugin_menu');
+
+//function my_plugin_menu() {
+	//add_options_page('Posts for Page Plugin Options', 'Posts for Page Plugin', 'manage_options', 'pfp-unique-identifier', 'my_plugin_options');
+//}
+
+function my_plugin_options() {
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	echo '<div class="wrap">';
+	echo '<p>How to use: In your page (HTML view) add shortcode e.g. [posts-in-page cat_slug=\'asp-net-programming-2\'] </p><p>This will get all posts with the category slug \'asp-net-programming-2\'</p> ';
+	echo '<p>post_id - Gets a post by ID, tag_slug - gets posts by tag slug, cat - gets posts with a category ID, cat_slug - gets posts with this category slug, author - gets posts by author, order_by - date or title, num - number of posts to show</p>';
+	echo '</div>';
+}
+
+add_action('wp_head', 'get_pfp_css');
+
+function get_pfp_css()
+{
+	echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/posts-for-page/pfp.css" />' . "\n";
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,112 @@
+=== Posts for Page ===
+Contributors: sihibbs
+Donate link: 
+Tags: post summary, posts in page, posts by tag, posts by category
+Requires at least: 3.3
+Tested up to: 3.4.2
+Stable tag: 1.7
+
+Include posts in a Page, by category, a selection of tags or by author with auto summary (read more) option. Supports pagination.
+
+== Description ==
+
+Include posts in a Page, by category, a selection of tags or by author with an auto summary (read more) option. Supports pagination when number of posts per page is specified.
+
+For example if you want a Page (in the main header navigation) to show a list of all posts in a category or set of tags and summarise the posts then this plugin will do it. Add a simple shortcode to the page (in HTML view) and the page is generated from the matching posts.
+
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Create (or edit) a page. Switch to HTML view.
+4. Insert the shortcode: [posts-for-page cat_slug='your-category-slug'] (to display all posts from this category)
+5. Add any additional options, it will default to 50 words with the read-more link or set show_full_posts='true' to show full post content.
+
+Example Usage:
+
+[posts-for-page cat_slug='asp-net-programming-2' hide_images='false' num='5' read_more='<br>Read More &raquo;' show_full_posts='false' use_wp_excerpt='true' strip_html='true' hide_post_content='false' show_meta='true']
+
+== Frequently Asked Questions ==
+<ul>
+<li>The first image in a post will be place at the start using the WP thumbnail if it is available. If images are a problem they can be hidden using: hide_images='true'</li>
+<li>Pagination is added if number of posts is specifified (i.e. num='3')</li>
+<li>"Read More" Text can be specified (i.e. read_more='More Here')</li>
+<li>Pagination link text can be specified using 'prev_text' and 'next_text'</li>
+</ul>
+<ul>
+	<li>Full Options:</li>
+    <li>cat_slug : Show all posts with this category slug</li>
+    <li>cat : Show all posts with a particular category ID</li>
+    <li>tag_slug : Show all posts with a specific tag (more than one can be entered, comma separated, i.e. tag_slug='html,css' (note: no spaces)</li>
+    <li>order_by : Order the posts by 'date' or 'title'</li>
+    <li>post_id : Show one post</li>
+    <li>author : Show posts by author</li>
+    <li>num : Limit the number of posts to show</li>
+    <li>hide_images (true/false) : If images are a problem they can be hidden using: hide_images='true'</li>
+    <li>Added in V1.5 (Pagination if number of posts is specified e.g. num='x')</li>
+    <li>read_more : Replace default read more text (i.e. read_more='Show me more...')</li>
+    <li>prev_text : Replace default Previous entry link text when specifying number of posts (i.e. prev_text='Newer')</li>
+    <li>next_text : Replace default Next entry link text when specifying number of posts (i.e. next_text='Older')</li>
+    <li>show_full_posts(true/false) : Show full post rather than excerpt (i.e. show_full_posts='true')</li>
+	<li>use_wp_excerpt - show the WP generated excerpt or the excerpt if defined for the post</li>
+	<li>strip_html : attempts to strip all HTML content (if not using use_wp_excerpt option)</li>
+	<li>hide_post_content : set to 'true' to only show the post titles</li>
+	<li>show_meta : set to 'true' to show post date and author name</li>
+	<li>hide_post_title : set to 'true' to hide the post title, e.g. if you just want to show one post using post_id</li>
+	<li>hide_read_more : set to 'true' to hide the read more link</li>
+	<li>create_para_tags : set to 'true' to use the WP 'wpautop' function to create &laquop&raquo tags and &laquobr&raquo tags from line breaks</li>
+</ul>
+<ul>
+	<li>Each summary posts is contained in a div with  class="pfpItem", so you can override styles by setting them in the WP stylesheet "styles.css". E.g ".div.pfpItem{font-size:20px;}"</li>
+</ul>
+== Screenshots ==
+
+1. `/postsforpage/1.0/screenshot-1.jpg`
+2. `/postsforpage/1.0/screenshot-1.jpg`
+
+== Changelog ==
+
+= 1.0 =
+* Release Version
+
+= 1.1 =
+* Update readme
+
+= 1.2 =
+* Fixed limit to number of posts which would be displayed
+
+= 1.3 =
+* Update readme
+
+= 1.4 =
+* Fixed header tag to be H2
+* Removes all images and inserts first image (if found) at start of post using WP Thumbnail version
+* If images are a problem they can be hidden using: hide_images='true'
+
+= 1.5 =
+* Pagination added if number of posts is specifified (i.e. num='3')
+* "Read More" Text can be specified (i.e. read_more='<br>More Here')
+* Pagination link text can be specified using 'prev_text' and 'next_text'
+* show_full_posts='true' - will show the full post content.
+
+= 1.6 =
+* Number of display issues resolved
+* new options added
+* use_wp_excerpt - set to 'true' to show the WP generated excerpt or the excerpt if defined for the post
+* strip_html - set to 'true' attempts to strip all HTML content (if not using use_wp_excerpt option)
+* hide_post_content - set to 'true' to only show the post titles
+* show_meta - set to 'true' to show post date and author name
+
+= 1.61 =
+* hide_post_title : set to 'true' to hide the post title, e.g. if you just want to show one post using post_id
+
+= 1.7 =
+* added create_para_tags 
+* fixed a bug with hide_read_more 
+
+== Upgrade Notice ==
+First Release
+
+
+
